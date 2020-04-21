994919f06567c0c95cb9f74717dbf29957428727
Mohammed Adinan <adinanaries@outlook.com>
2020-04-20 20:33:47 -0400

    added a static folder for images static assets

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public", "images")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
