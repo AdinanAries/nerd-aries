8da8a2b7b1b8fcd44f4bb9acb42d9e793f12d66d
Mohammed Adinan <adinanaries@outlook.com>
2020-04-20 20:07:33 -0400

    added login and signup pages

--- public/script.js
+++ public/script.js
@@ -2,67 +2,105 @@ function showHomePage() {
   $("#GoBackBtn").css("display", "none");
   $(".NavLink").removeClass("active");
   $("#HomeLink").addClass("active");
+  document.getElementById("loginPage").style.display = "none";
   document.getElementById("WebsitesPage").style.display = "none";
   document.getElementById("VlogsPage").style.display = "none";
   document.getElementById("TutoringPage").style.display = "none";
   document.getElementById("MentoringPage").style.display = "none";
   document.getElementById("PatreonsPage").style.display = "none";
+  document.getElementById("signupPage").style.display = "none";
   document.getElementById("dashboard").style.display = "block";
 }
 function showWebSitesPage() {
   $("#GoBackBtn").css("display", "block");
   $(".NavLink").removeClass("active");
   $("#WebsitesLink").addClass("active");
+  document.getElementById("loginPage").style.display = "none";
   document.getElementById("dashboard").style.display = "none";
   document.getElementById("VlogsPage").style.display = "none";
   document.getElementById("TutoringPage").style.display = "none";
   document.getElementById("MentoringPage").style.display = "none";
   document.getElementById("PatreonsPage").style.display = "none";
+  document.getElementById("signupPage").style.display = "none";
   document.getElementById("WebsitesPage").style.display = "block";
 }
 function showVlogsPage() {
   $("#GoBackBtn").css("display", "block");
   $(".NavLink").removeClass("active");
   $("#VlogsLink").addClass("active");
+  document.getElementById("loginPage").style.display = "none";
   document.getElementById("dashboard").style.display = "none";
   document.getElementById("WebsitesPage").style.display = "none";
   document.getElementById("TutoringPage").style.display = "none";
   document.getElementById("MentoringPage").style.display = "none";
   document.getElementById("PatreonsPage").style.display = "none";
+  document.getElementById("signupPage").style.display = "none";
   document.getElementById("VlogsPage").style.display = "block";
 }
 function showTutoringPage() {
   $("#GoBackBtn").css("display", "block");
   $(".NavLink").removeClass("active");
   $("#TutoringLink").addClass("active");
+  document.getElementById("loginPage").style.display = "none";
   document.getElementById("dashboard").style.display = "none";
   document.getElementById("WebsitesPage").style.display = "none";
   document.getElementById("VlogsPage").style.display = "none";
   document.getElementById("MentoringPage").style.display = "none";
   document.getElementById("PatreonsPage").style.display = "none";
+  document.getElementById("signupPage").style.display = "none";
   document.getElementById("TutoringPage").style.display = "block";
 }
 function showMentoringPage() {
   $("#GoBackBtn").css("display", "block");
   $(".NavLink").removeClass("active");
   $("#MentoringLink").addClass("active");
+  document.getElementById("loginPage").style.display = "none";
   document.getElementById("dashboard").style.display = "none";
   document.getElementById("WebsitesPage").style.display = "none";
   document.getElementById("VlogsPage").style.display = "none";
   document.getElementById("TutoringPage").style.display = "none";
   document.getElementById("PatreonsPage").style.display = "none";
+  document.getElementById("signupPage").style.display = "none";
   document.getElementById("MentoringPage").style.display = "block";
 }
 function showPatreonsPage() {
   $("#GoBackBtn").css("display", "block");
   $(".NavLink").removeClass("active");
   $("#PatreonsLink").addClass("active");
+  document.getElementById("loginPage").style.display = "none";
   document.getElementById("dashboard").style.display = "none";
   document.getElementById("WebsitesPage").style.display = "none";
   document.getElementById("VlogsPage").style.display = "none";
   document.getElementById("TutoringPage").style.display = "none";
   document.getElementById("MentoringPage").style.display = "none";
   document.getElementById("PatreonsPage").style.display = "block";
+  document.getElementById("signupPage").style.display = "none";
+}
+function showLoginPage() {
+  $("#GoBackBtn").css("display", "block");
+  $(".NavLink").removeClass("active");
+  $("#HomeLink").addClass("active");
+  document.getElementById("loginPage").style.display = "block";
+  document.getElementById("dashboard").style.display = "none";
+  document.getElementById("WebsitesPage").style.display = "none";
+  document.getElementById("VlogsPage").style.display = "none";
+  document.getElementById("TutoringPage").style.display = "none";
+  document.getElementById("PatreonsPage").style.display = "none";
+  document.getElementById("MentoringPage").style.display = "none";
+  document.getElementById("signupPage").style.display = "none";
+}
+function showSignupPage() {
+  $("#GoBackBtn").css("display", "block");
+  $(".NavLink").removeClass("active");
+  $("#HomeLink").addClass("active");
+  document.getElementById("signupPage").style.display = "block";
+  document.getElementById("loginPage").style.display = "none";
+  document.getElementById("dashboard").style.display = "none";
+  document.getElementById("WebsitesPage").style.display = "none";
+  document.getElementById("VlogsPage").style.display = "none";
+  document.getElementById("TutoringPage").style.display = "none";
+  document.getElementById("PatreonsPage").style.display = "none";
+  document.getElementById("MentoringPage").style.display = "none";
 }
 
 function showWebsitesPageContent() {
@@ -127,4 +165,31 @@ $("#toBookTutoringFormBtn").click(function (event) {
 
 $("#BacktoTutoringIntroBtn").click(function (event) {
   showtutoringPageIntro();
+});
+
+$(".loginBtn").click(function (event) {
+  showLoginPage();
+});
+$("#signupLink").click(function (event) {
+  showSignupPage();
+});
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 70) {
+    let TopNavBarMenu = document.getElementById("TopNavBarMenu");
+    TopNavBarMenu.style.position = "fixed";
+    /*TopNavBarMenu.style.transition = "top 2s";*/
+    TopNavBarMenu.style.left = "0";
+    TopNavBarMenu.style.top = "0";
+    TopNavBarMenu.style.width = "100vw";
+    TopNavBarMenu.style.backgroundColor = "white";
+  } else {
+    let TopNavBarMenu = document.getElementById("TopNavBarMenu");
+    /*TopNavBarMenu.style.transition = "top 2s";*/
+    TopNavBarMenu.style.position = "initial";
+    TopNavBarMenu.style.left = "0";
+    TopNavBarMenu.style.top = "0";
+    TopNavBarMenu.style.width = "auto";
+    TopNavBarMenu.style.background = "none";
+  }
 });
