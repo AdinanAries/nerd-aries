bdd4f7bd647ab3130831e7e7334075d22044655c
Mohammed Adinan <adinanaries@outlook.com>
2020-04-24 11:17:10 -0400

    removed overflowing arrows for carousel when display-width equals mobile

--- public/script.js
+++ public/script.js
@@ -210,9 +210,47 @@ $(window).scroll(function () {
 
 $(document).ready(function () {
   $(".ClientReviews").slick({
+    slide: ".eachReview",
+    lazyLoad: "ondemand",
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
   });
+});
+
+$(".responsive").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 });
