4b2fef19988b9716de000d7c0b5ee62a932a7575
Mohammed Adinan <adinanaries@outlook.com>
2024-12-25 13:03:31 -0500

    deferring gif images in order to improve load time

--- public/script.js
+++ public/script.js
@@ -535,6 +535,18 @@ function RegisterStudent(callback) {
     },
   });
 }
+
+function init() {
+  let imgDefer = document.querySelectorAll('img.defer');
+  for (let i = 0; i < imgDefer.length; i++) {
+    if (imgDefer[i].getAttribute('data-src')) {
+      imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
+    }
+  }
+}
+
+window.onload = init;
+
 /**
 document.getElementById("TutoringSubmitBtn").addEventListener("click", () => {
   if (
